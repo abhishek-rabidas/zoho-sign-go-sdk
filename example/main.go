715668f9489fed49b221285dda7eca98f72ecc8c
abhishek.rabidas <abhishek.rabidas@synlabs.io>
2025-04-05 22:07:42 +0530

    example file add

---
+++ example/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	zohosign "github.com/abhishek-rabidas/zoho-sign-go-sdk"
+	"github.com/charmbracelet/log"
+)
+
+func main() {
+	zohoSignClient := zohosign.NewZohoSignClient("your_refresh_token", "your_client_id", "your_secret_key", 5)
+
+	// Sending a template signature request
+	res, err := zohoSignClient.CreateTemplateSignRequest(
+		"recipient_name",
+		"recipient_email",
+		"recipient_phone_number",
+		"recipient_country_code",
+		"template_id",
+		"action_id",
+		"role",
+		"notes",
+		true,
+	)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Infof("Template Signature Request Response: %+v", res)
+
+	// Checking the status of a document
+	status, err := zohoSignClient.CheckDocumentStatus(res.Requests.RequestId)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Infof("Document Status: %+v", status)
+
+	// Get Embedded Signature URL
+	embeddedSignResponse, err := zohoSignClient.GetEmbeddedSignatureURL(res.Requests.RequestId, res.Requests.Actions[0].ActionId)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Infof("Embedded Signature URL: %s", embeddedSignResponse.SignUrl)
+
+	// Cancel a document
+	err = zohoSignClient.CancelSignatureRequest(res.Requests.RequestId)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	log.Info("Document cancelled successfully")
+
+}
